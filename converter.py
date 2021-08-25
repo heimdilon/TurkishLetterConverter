37363a6d1f1da83195719bf28f9ead5fd09993a6
Orkun Koray GÜNGÖR <73837799+heimdilon@users.noreply.github.com>
2021-08-25 09:53:43 +0300

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,21 @@
+# Created by kor_a at 25/08/2021
+unwanted_letters = ["ş", "ı", "ğ", "ü", "ö", "ç",
+                    "İ", "Ş", "Ü", "Ö", "Ğ", "Ç"]
+
+wanted_letters = ["s", "i", "g", "u", "o", "c",
+                  "I", "S", "U", "O", "G", "C"]
+
+sentence = input("Write what do you want : ")
+
+def changeLetters(sentence):
+    letters = [x for x in sentence]
+    for letter in letters:
+        if letter in unwanted_letters:
+            letter_index = letters.index(f"{letter}")
+            unwanted_letter_index = unwanted_letters.index(f"{letter}")
+            letters[letter_index] = wanted_letters[unwanted_letter_index]
+        new_sentence = "".join(letters)
+    print(new_sentence)
+
+
+changeLetters(sentence)
